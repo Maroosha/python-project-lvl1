d69e2478314f1d16d38da28925b2ba8a7c2b190d
Mariia Bilinska <mariiaklochko@gmail.com>
2021-09-30 09:43:56 +0100

    Edit brain_games.py and setup.cfg, step4

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -2,15 +2,12 @@
 
 # !/usr/bin/env python3
 
-import logging
-
 from brain_games.cli import welcome_user
 
 
 def main():
     """Welcome the user."""
-    log = logging.getLogger()
-    log.info('Welcome to the Brain Games!')
+    print('Welcome to the Brain Games!')
     return welcome_user()
 
 
