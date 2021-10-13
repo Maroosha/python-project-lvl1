3e85049334cdbfd8be6b71cce3740ded00f47908
Mariia Bilinska <mariiaklochko@gmail.com>
2021-10-13 10:04:47 +0100

    Edit brain_games/engine.py: comments 5,12

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -4,13 +4,16 @@
 
 import prompt
 
+ATTEMPT_NO = 3
 
-def run_engine(game_function, game_rule):
+
+def run_engine(game_function, game_rule, attempts=ATTEMPT_NO):
     """Brain Games engine. Play untill 3 correct answers or 1 faillure.
 
     Parameters:
         game_function: game function;
-        game_rule: game rule.
+        game_rule: game rule;
+        attempts: max number of attempts (constant).
 
     Returns:
         nothing.
@@ -20,21 +23,18 @@ def run_engine(game_function, game_rule):
     name = prompt.string('May I have your name? ')
     print('Hello, {0}!'.format(name))
     print(game_rule)
-    count = 0
-    while count < 3:
+    for _ in range(attempts):
         question, corr = game_function()
         print('Question: {0}'.format(question))
         answer = prompt.string('Your answer: ')
         if answer == corr:
             print('Correct!')
-            count += 1
         else:
             print(
                 '"{0}" is wrong answer ;( '
                 'Correct answer was "{1}".'.format(answer, corr),
             )
             print("Let's try again, {0}!".format(name))
-            break
-    if count == 3:
-        print('Congratulations, {0}!'.format(name))
+            return
+    print('Congratulations, {0}!'.format(name))
     return
